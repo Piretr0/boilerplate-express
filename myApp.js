cafbb186c69d19ea3bbe502bfd917eece4ad785f
Karol Mikołajewski <karolmikolajewski@wp.pl>
2025-09-13 14:12:47 +0200

    end

--- myApp.js
+++ myApp.js
@@ -1,36 +1,50 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+// app.get("/", function(req, res){
+//     console.log("Hello Express");
+//   res.send('Hello Express');
+// });
+process.env.MESSAGE_STYLE = "uppercase";
+require('dotenv').config()
 
-console.log("Hello, World");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(function(req, res, next) {
+ console.log(req.method +" "+ req.path +" - "+ req.ip);
+  next();
+})
+
+app.get("/now", function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({time: req.time});
+});
+
+app.get("/:word/echo", function(req, res) {
+  res.json({echo: req.params.word});
+});
+
+app.get("/name", function(req, res) {
+  let firstName = req.query.first;
+  let lastName = req.query.last;
+  res.json({name: firstName + " " + lastName});
+});
 
 
+app.use(bodyParser.urlencoded({ extended: false }))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.post("/name", function(req, res) {
+  let firstName = req.body.first;
+  let lastName = req.body.last;
+  res.json({name: firstName + " " + lastName});
+});
 
 
 
